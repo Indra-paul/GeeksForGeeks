0f09450cef5c1f36837d99bcd0c76151256c6920
Indrayudh Paul <paulindrayudh@gmail.com>
2023-07-24 22:41:24 +0530

    20230724 POTD

---
+++ POTD Problem Of The Day/P20230724RightViewOfBinaryTree.java
@@ -0,0 +1,189 @@
+/*
+Given a Binary Tree, find Right view of it. Right view of a Binary Tree is set of nodes visible when tree is viewed from right side.
+
+Right view of following tree is 1 3 7 8.
+
+          1
+       /     \
+     2        3
+   /   \      /    \
+  4     5   6    7
+    \
+     8
+
+Example 1:
+
+Input:
+       1
+    /    \
+   3      2
+Output: 1 2
+Example 2:
+
+Input:
+     10
+    /   \
+  20     30
+ /   \
+40  60 
+Output: 10 30 60
+Your Task:
+Just complete the function rightView() that takes node as parameter and returns the right view as a list. 
+
+Expected Time Complexity: O(N).
+Expected Auxiliary Space: O(Height of the Tree).
+
+Constraints:
+1 ≤ Number of nodes ≤ 105
+0 ≤ Data of a node ≤ 105
+*/
+
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+
+class P20230724RightViewOfBinaryTree {
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    void inOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+ 
+        inOrder(node.left);
+        System.out.print(node.data + " ");
+ 
+        inOrder(node.right);
+    }
+    
+	public static void main (String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	
+		String s = br.readLine();
+		Node root = buildTree(s);
+		Solution tree = new Solution();
+		ArrayList<Integer> arr = tree.rightView(root);
+		for(int x : arr)
+		System.out.print(x +" ");
+		System.out.println();
+
+	}
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+/*Complete The Function Provided
+Given Below is The Node Of Tree
+class Node
+{
+    int data;
+    Node left, right;
+    public Node(int data)
+    {
+        this.data = data;
+         left = right = null;
+    }
+}*/
+
+
+class Solution{
+    //Function to return list containing elements of right view of binary tree.
+    ArrayList<Integer> rightView(Node node) {
+        //add code here.
+        
+        ArrayList<Integer> rsv = new ArrayList<Integer>();
+        int level = 0;
+        recRightView(node,rsv,level);
+        
+        return rsv;
+        
+    }
+    
+    public static void recRightView(Node head, ArrayList<Integer> rsv, int level){
+        if(head==null)
+            return;
+        
+        if(level==rsv.size())
+            rsv.add(head.data);
+            
+        recRightView(head.right, rsv, level+1);
+        recRightView(head.left, rsv, level+1);
+    }
+}
+
